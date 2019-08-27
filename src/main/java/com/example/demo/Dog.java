52bf45aba8f4badbeafcf233d498140cf2e74b4a
ThomasHouze <mrbugsbunny1st@gmail.com>
2019-08-27 10:24:16 -0400

    exercise 3.01 from week 26

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String Name;
+
+    @NotNull
+    @Size(min=1, max=50)
+    private int Age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+}
